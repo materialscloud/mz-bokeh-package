2c0e72ea8776b466c9143d7444b0a134d591a603
roiweinreb <roiweinreb@gmail.com>
2021-06-10 08:27:08 +0300

    add imports to "utilities/__init__.py"

--- mz_bokeh_package/utilities/__init__.py
+++ mz_bokeh_package/utilities/__init__.py
@@ -1,2 +1,4 @@
+from .auth import Auth, CurrentUser  # noqa F401
+from .environment import Environment  # noqa F401
 from .bokeh_utilities import BokehUtilities  # noqa F401
 from .materials_zone_api import MaterialsZoneApi  # noqa F401
