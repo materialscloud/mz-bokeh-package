a2a5e16203639d9f1fa5d58c9d85c6bdcc6b89d9
roiweinreb <roiweinreb@gmail.com>
2021-06-10 08:30:07 +0300

    removed custom widgets from the components package

--- mz_bokeh_package/components/__init__.py
+++ mz_bokeh_package/components/__init__.py
@@ -1,3 +1,2 @@
-from ..custom_widgets import CustomMultiSelect, CustomSelect  # noqa F401
 from .app_state import AppState, AppStateValue  # noqa F401
 from .loading_spinner import LoadingSpinner  # noqa F401
