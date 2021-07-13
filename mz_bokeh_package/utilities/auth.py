c504da46f896ded43e1c91708e05b2bf7fdee233
oriyudilevich <oriyudilevich@gmail.com>
2021-07-13 15:12:27 +0200

    fixed bug that Bokeh auth methods should not be part of a class

--- mz_bokeh_package/utilities/auth.py
+++ mz_bokeh_package/utilities/auth.py
@@ -12,60 +12,61 @@ from typing import Optional
 from mz_bokeh_package.utilities.environment import Environment
 
 
+def get_user(request_handler: RequestHandler) -> str:
+    """
+    authenticate user based on api_key and user_key that are sent via the query parameters of the request and
+    return the user_id if the user is authenticated and otherwise return None
+
+    Args:
+        request_handler: a tornado RequestHandler object that contains the query parameters of the request,
+            which contains the api_key and user_key of the user
+
+    Returns:
+        the user id when authentication is successful, and otherwise None
+    """
+
+    # bypass authentication in the case of health.py dashboard to allow GCP to perform health checks
+    if request_handler.request.path.split("/")[-1] in ("health", "error"):
+        return "ok"
+
+    query_arguments = request_handler.request.query_arguments
+
+    # get the api_key from the request header
+    api_keys = query_arguments.get("api_key")
+    if api_keys is not None and len(api_keys) == 1:
+        api_key = api_keys[0]
+    else:
+        api_key = ""
+
+    # get the user_key from the request header
+    user_keys = query_arguments.get("user_key")
+    if user_keys is not None and len(user_keys) == 1:
+        user_key = user_keys[0]
+    else:
+        user_key = ""
+
+    # in the development environment, allow overriding the api_key and user_key via env variables
+    if Environment.get_environment() == 'dev':
+        api_key = os.getenv('API_KEY', api_key)
+        user_key = os.getenv('USER_KEY', user_key)
+
+    # authenticate the user using the MaterialsZone API by requesting the user_id
+    user_id = Auth.get_user_from_api_key(api_key, user_key)
+
+    return user_id
+
+
+def get_login_url(request_handler: RequestHandler) -> str:
+    """gets the login url for failed authentication depending on the environment
+
+    Returns:
+        the MZ App URL (environment dependant) to redirect to if authentication fails
+    """
+
+    return Environment.get_error_page_url()
+
+
 class Auth:
-
-    def get_user(self, request_handler: RequestHandler) -> str:
-        """
-        authenticate user based on api_key and user_key that are sent via the query parameters of the request and
-        return the user_id if the user is authenticated and otherwise return None
-
-        Args:
-            request_handler: a tornado RequestHandler object that contains the query parameters of the request,
-                which contains the api_key and user_key of the user
-
-        Returns:
-            the user id when authentication is successful, and otherwise None
-        """
-
-        # bypass authentication in the case of health.py dashboard to allow GCP to perform health checks
-        if request_handler.request.path.split("/")[-1] in ("health", "error"):
-            return "ok"
-
-        query_arguments = request_handler.request.query_arguments
-
-        # get the api_key from the request header
-        api_keys = query_arguments.get("api_key")
-        if api_keys is not None and len(api_keys) == 1:
-            api_key = api_keys[0]
-        else:
-            api_key = ""
-
-        # get the user_key from the request header
-        user_keys = query_arguments.get("user_key")
-        if user_keys is not None and len(user_keys) == 1:
-            user_key = user_keys[0]
-        else:
-            user_key = ""
-
-        # in the development environment, allow overriding the api_key and user_key via env variables
-        if Environment.get_environment() == 'dev':
-            api_key = os.getenv('API_KEY', api_key)
-            user_key = os.getenv('USER_KEY', user_key)
-
-        # authenticate the user using the MaterialsZone API by requesting the user_id
-        user_id = self.get_user_from_api_key(api_key, user_key)
-
-        return user_id
-
-    @staticmethod
-    def get_login_url(request_handler: RequestHandler) -> str:
-        """gets the login url for failed authentication depending on the environment
-
-        Returns:
-            the MZ App URL (environment dependant) to redirect to if authentication fails
-        """
-
-        return Environment.get_error_page_url()
 
     @staticmethod
     def get_user_from_api_key(api_key: str, user_key: str) -> Optional[str]:
