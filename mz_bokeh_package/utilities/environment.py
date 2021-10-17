c67cfb55405da73082d109cd787253a60e2508dc
ElanaRB <elana@rothsteins.org>
2021-10-17 13:46:04 +0300

    correct staging url

--- mz_bokeh_package/utilities/environment.py
+++ mz_bokeh_package/utilities/environment.py
@@ -75,8 +75,8 @@ class Environment:
         env = cls.get_environment()
 
         if env == "staging":
-            return "materials-zone-v2.firebaseapp.com"
+            return "app-staging.materials.zone"
         elif env == "production":
             return "app.materials.zone"
         elif env == "dev":
-            return os.getenv('WEBAPP_HOST', "materials-zone-v2.firebaseapp.com")
+            return os.getenv('WEBAPP_HOST', "app-staging.materials.zone")
