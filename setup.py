75844b38aaef04fa01f054d09fe7a2271e15907e
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2022-10-12 08:41:45 +0300

    version update following rebasing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mz_bokeh_package",
-    version="0.12.2",
+    version="0.12.3",
     packages=["mz_bokeh_package"],
     include_package_data=True,
 
