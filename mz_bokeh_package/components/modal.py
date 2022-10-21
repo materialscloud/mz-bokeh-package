c982a5fe7e377776fb4969f0971c132ea2bbe877
roiweinreb <roiweinreb@gmail.com>
2022-10-21 13:43:53 -0400

    display the modal via backend method

--- mz_bokeh_package/components/modal.py
+++ mz_bokeh_package/components/modal.py
@@ -12,7 +12,7 @@ CONFIRMATION_MODAL_MACROS_PATH = os.path.join(BASE_DIR, "../templates/confirmati
 
 
 class ConfirmationModal:
-    def __init__(self, title: str, invoking_widget: Widget, content_widgets: List[Widget]):
+    def __init__(self, title: str, content_widgets: List[Widget]):
         """This class creates a confirmation modal.
 
         In addition to instantiating this class, it's also required to include the
@@ -20,29 +20,17 @@ class ConfirmationModal:
         Currently, the modal can only be invoked by a Button Bokeh widget.
 
         Args:
-            title (str): Modal's title.
-            invoking_widget (Widget): The Bokeh widget that invokes this modal.
-            content_widgets (List[Widget]): The Bokeh widgets that should be displayed in the
+            title: Modal's title.
+            content_widgets: The Bokeh widgets that should be displayed in the
                 main area of the modal.
         """
         self._title = title
-        self._invoking_widget = invoking_widget
         self._content_widgets = content_widgets
         self._invoker_css_class = "confirmation-modal-invoker"
 
-        # Create a dummy widget to allow invoking a backend callback (python)
-        # from the frontend (javascript).
-        self._backend_callback_invoker = Toggle()
-        self._backend_callback_invoker.on_change("active", self._on_modal_invocation)
-
-        # Add a class name to the modal invoker.
-        self._invoking_widget.css_classes += [self._invoker_css_class]
-
-        # Set a Javascript callback to allow:
-        #   - Invoking the modal
-        #   - Dismissing the modal when clicking apply/cancel
-        #   - Run a backend callback on modal invocation
-        self._invoking_widget.js_on_click(self._get_js_callback())
+        # Create a dummy widget to allow invoking the modal from the backend.
+        self._modal_invoker = Toggle(visible=False, name="modal_invoker")
+        self._modal_invoker.js_on_change("active", self._get_modal_invoker_js_callback())
 
         # "Apply" button.
         self._apply_modal_btn = Button(
@@ -65,29 +53,28 @@ class ConfirmationModal:
         # Layout the widgets.
         self.layout = Column(
             *(widget for widget in self._content_widgets),
+            self._modal_invoker,
             self._apply_modal_btn,
             self._cancel_modal_btn,
         )
 
         self._configure_jinja_environment()
 
-    def _get_js_callback(self) -> CustomJS:
-        """Returns a javascript callback to run when a button is clicked.
+    def _get_modal_invoker_js_callback(self) -> CustomJS:
+        """Returns a javascript callback to run when the modal invoker is triggered.
 
         Returns:
             CustomJS: "on_click" Javascript callback.
         """
-
-        args = {"backend_callback_invoker": self._backend_callback_invoker}
-        code = f"""
+        code = """
         // Add "data" attribute to both the "Apply" and "Cancel" buttons (in the modal) to allow closing the modal.
         $(".apply-btn > .bk-btn-group > .bk-btn").attr("data-dismiss", "modal")
         $(".cancel-btn > .bk-btn-group > .bk-btn").attr("data-dismiss", "modal")
 
-        // Invoke backend callback
-        backend_callback_invoker.active = !backend_callback_invoker.active
+        // Show the modal.
+        $('#confirmation-modal').modal('show')
         """
-        return CustomJS(args=args, code=code)
+        return CustomJS(code=code)
 
     def _configure_jinja_environment(self):
         """Configures the Bokeh template's environment.
@@ -110,10 +97,13 @@ class ConfirmationModal:
     def _on_apply_modal(self, event):
         """This function runs when the "Apply" button is clicked.
         """
-        logger.info('The "Apply" button was clicked.')
+        logger.debug('The "Apply" button was clicked.')
 
     def _on_cancel_modal(self, event):
-        logger.info('The "Cancel" button was clicked.')
+        logger.debug('The "Cancel" button was clicked.')
 
-    def _on_modal_invocation(self, attr, old, new):
-        logger.info('The modal was invoked.')
+    def show(self):
+        # Change the "active" property in order to trigger the
+        # JS callback that displays the modal.
+        self._modal_invoker.active = not self._modal_invoker.active
+        logger.debug("The modal was displayed.")
