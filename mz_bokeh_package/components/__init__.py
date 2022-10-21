ba07b43a62810eb5caf026552c573b58c9b2b2ea
roiweinreb <roiweinreb@gmail.com>
2022-10-21 13:48:16 -0400

    Imported modal class in the "__init__" file

--- mz_bokeh_package/components/__init__.py
+++ mz_bokeh_package/components/__init__.py
@@ -1,3 +1,4 @@
 from .app_state import AppState, AppStateValue  # noqa F401
 from .loading_spinner import LoadingSpinner  # noqa F401
 from .plot_settings import PlotSettings  # noqa F401
+from .confirmation_modal import ConfirmationModal  # noqa F401
