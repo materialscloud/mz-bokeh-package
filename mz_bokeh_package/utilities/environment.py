a7a02b221725f01ee91bf142be775240824410e1
oriyudilevich <oriyudilevich@gmail.com>
2023-03-10 13:28:10 +0100

    The "get" methods of the Environment class now always fetch the host from the environment variables. Upgrade version and versions of requests library.

--- mz_bokeh_package/utilities/environment.py
+++ mz_bokeh_package/utilities/environment.py
@@ -8,13 +8,13 @@ class Environment:
 
     @staticmethod
     def get_environment() -> str:
-        """get the current environment
-
-        Raises:
-            ValueError: Whenever the environment is invalid.
+        """Get the current environment.
 
         Returns:
             str: the current environment, possible values are: 'dev', 'staging' or 'production'
+
+        Raises:
+            ValueError: Whenever the environment is invalid.
         """
 
         # in the kubernetes deployed containers, the environemnt variable ENVIRONMENT is set to staging/production
@@ -27,75 +27,71 @@ class Environment:
 
     @classmethod
     def get_request_url(cls, endpoint: str) -> str:
-        """receives an endpoint of an API request and converts it to a request url based on the environment.
-        If the environment variable 'ENVIRONMENT' is set to 'dev' the environment variable 'API_HOST' should
-        be set to the desired url, including 'https://'. If 'API_HOST' is not set in the development environment,
-        the default staging url will be returned.
+        """Converts an endpoint of an API request to a request url. The host should be set in the environment variable
+        'API_HOST'.
 
         Args:
             endpoint: the endpoint of the request
 
         Returns:
             the full URL of the request
+
+        Raises:
+            ValueError: When the environment variable `API_HOST` is not set.
         """
-
-        env = cls.get_environment()
-
-        if env == 'staging':
-            host = 'https://api-staging.materials.zone/v1beta1'
-        elif env == 'production':
-            host = 'https://api.materials.zone/v1beta1'
-        elif env == 'dev':
-            host = os.getenv('API_HOST', 'https://api-staging.materials.zone/v1beta1')
-
+        host = Environment._get_host_from_env_var('API_HOST')
         return f"{host}/{endpoint}"
 
     @classmethod
     def get_graphql_api_url(cls) -> str:
-        """Returns the url of the GraphQL API Server.
-        If the environment variable 'ENVIRONMENT' is set to 'dev' the environment variable 'GRAPHQL_API_HOST' should
-        be set to the desired url, including 'https://'. If 'GRAPHQL_API_HOST' is not set in the development
-        environment, the default staging url will be returned.
+        """Returns the url of the GraphQL API Server. The host should be set in the environment variable
+        'GRAPHQL_API_HOST'.
 
         Returns:
             the full URL of the GraphQL API server
+
+        Raises:
+            ValueError: When the environment variable `GRAPHQL_API_HOST` is not set.
         """
-
-        env = cls.get_environment()
-
-        if env == 'staging':
-            host = 'https://api-staging.materials.zone/graphql'
-        elif env == 'production':
-            host = 'https://api.materials.zone/graphql'
-        elif env == 'dev':
-            host = os.getenv('GRAPHQL_API_HOST', 'https://api-staging.materials.zone/graphql')
-
+        host = Environment._get_host_from_env_var('GRAPHQL_API_HOST')
         return f"{host}"
 
     @classmethod
     def get_parser_service_url(cls, endpoint: str) -> str:
-        """receives an endpoint of a Parser Service request and converts it to a request url based on the environment.
-        If the environment variable 'ENVIRONMENT' is set to 'dev' the environment variable 'PARSER_SERVICE_HOST' should
-        be set to the desired url, including 'https://'. If 'PARSER_SERVICE_HOST' is not set in the development
-        environment, the default staging url will be returned.
+        """Converts an endpoint of the Parser Service to a request url. The host should be set in the environment
+        variable 'PARSER_SERVICE_HOST'.
 
         Args:
             endpoint: the endpoint of the request
 
         Returns:
             the full URL of the request
+
+        Raises:
+            ValueError: When the environment variable `PARSER_SERVICE_HOST` is not set.
         """
 
-        env = cls.get_environment()
-
-        if env == 'staging':
-            host = 'https://parser-service-staging.materials.zone/api/v1beta1'
-        elif env == 'production':
-            host = 'https://parser-service.materials.zone/api/v1beta1'
-        elif env == 'dev':
-            host = os.getenv('PARSER_SERVICE_HOST', 'https://parser-service-staging.materials.zone/api/v1beta1')
-
+        host = Environment._get_host_from_env_var('PARSER_SERVICE_HOST')
         return f"{host}/{endpoint}"
+
+    @classmethod
+    def get_webapp_host(cls) -> str:
+        """Get the web app host as set in the environment variable WEBAPP_HOST.
+
+        Returns:
+            str: Web app host.
+
+        Raises:
+            ValueError: When the environment variable `WEBAPP_HOST` is not set.
+        """
+        return Environment._get_host_from_env_var('WEBAPP_HOST')
+
+    @classmethod
+    def _get_host_from_env_var(cls, env_var_name: str):
+        host = os.getenv(env_var_name)
+        if not host:
+            raise ValueError(f'The {env_var_name} environment variable is not set.')
+        return host
 
     @classmethod
     def get_error_page_url(cls) -> str:
@@ -111,19 +107,3 @@ class Environment:
             return "https://bokeh-staging.materials.zone/error"
         elif env == 'production':
             return "https://bokeh.materials.zone/error"
-
-    @classmethod
-    def get_webapp_host(cls) -> str:
-        """get the web app host based on the environment.
-
-        Returns:
-            str: Web app host.
-        """
-        env = cls.get_environment()
-
-        if env == "staging":
-            return "https://app-staging.materials.zone"
-        elif env == "production":
-            return "https://app.materials.zone"
-        elif env == "dev":
-            return os.getenv('WEBAPP_HOST', "https://app-staging.materials.zone")
