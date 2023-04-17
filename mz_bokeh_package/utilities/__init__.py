8bf7356be8687ae87edc8381c93dad2872d62b4a
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2023-04-17 16:02:52 +0300

    Update __init__.py

--- mz_bokeh_package/utilities/__init__.py
+++ mz_bokeh_package/utilities/__init__.py
@@ -1,4 +1,5 @@
-from .auth import Auth, CurrentUser  # noqa F401
+from .auth import CurrentUser  # noqa F401
+from .graphql_api import MZGraphQLClient  # noqa F401
 from .environment import Environment  # noqa F401
 from .bokeh_utilities import BokehUtilities  # noqa F401
 from .materials_zone_api import MaterialsZoneApi  # noqa F401
