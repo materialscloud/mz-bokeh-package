0ebc9e6c3bae5e68f22e2023ecf6c6abcdf4c83e
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2023-05-02 12:47:44 +0300

    MZGraphQLClient: replace e.message with e

--- mz_bokeh_package/utilities/graphql_api.py
+++ mz_bokeh_package/utilities/graphql_api.py
@@ -64,13 +64,13 @@ class MZGraphQLClient:
             result = client.execute(query)
         except RetryError as e:
             raise GraphqlQueryError(f"invalid result of the viewer GraphQL query. The provided API key may be invalid"
-                                    f"Retry error: {e.message}")
+                                    f"Retry error: {e}")
 
         try:
             validate(result, schema=result_schema)
         except ValidationError as e:
             raise GraphqlQueryError(f"invalid result of the viewer GraphQL query."
-                                    f"Validation error: {e.message}")
+                                    f"Validation error: {e}")
 
         return result['viewer']
 
