962ba63c79af45c7c6bc8f2f029ae64646a02daf
captKuk <mail@skudera.de>
2023-07-10 11:43:47 -0400

    Bump version to 0.17.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mz_bokeh_package",
-    version="0.17.0",
+    version="0.17.1",
     packages=["mz_bokeh_package"],
     include_package_data=True,
 
